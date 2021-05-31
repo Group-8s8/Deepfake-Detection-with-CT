5fad4037f006c9cf2b4da3e59f97daa183b44528
Nuzair Rasheed <38219313+Nuzair46@users.noreply.github.com>
2021-05-31 14:34:12 +0530

    m step
    more fix for e step and a part of m step is written. This takes a really long time to go through an image. 128x128 pixel image took 5 minutes to complete the e step.

--- utils/expect.py
+++ utils/expect.py
@@ -26,33 +26,32 @@ class EM():
 
 		sigma = SIGMAZERO 
 
-		for n in range(1,100):
+		for n in range(1,2): #update 2 to 100 later
 			print("in loop")
 			#e-step
+			j=0
 			for x,y in COORDINATES:
-				isum = 0
-				for p in range(-ALPHA,ALPHA):
-					try:
-						img[x + p][y + p]
-					except IndexError:
-						# this error is raised when kernel reaches an edge of the image, stopping the loop.
-						break
-					# this is the Equation No. 1 from the paper.
-					isum += K[p][p] * img[x + p][y + p]
-
+				isum = EM.convolve(img, x, y, ALPHA, K)
 				temp = img[x][y] - isum
+
+				'''
 				for i in range(0,len(temp)):
 					if temp[i] < 0:
 						temp[i] = temp[i] * -1 
-				R[x][y] = temp
-				#gd eq
-				P[x][y] = GD.guassian_distribution(R[x][y], sigma)
-				W[x][y] = P[x][y]/(P[x][y] + PZERO)
-			print(W)
-			print(n)
-			sigma = sigma + 1
-		 # Why is everything 0
-		'''
+				'''
+				R = temp
+				P = GD.guassian_distribution(R, sigma)
+				#print(P)
+				try:
+					W = P/(P + PZERO)
+				except IndexError:
+					pass
+				#print(W)
+				j = j + 1
+				print(j)
+				if j == 100: # just a limit for the loop so its easy to test.
+					break
+					
 			#m-step
 			
 			sigmasqr = 1 #"Some Value is calcuted here to give the varience (sigma square)" # DONT KNOW HOW THIS IS CALCULATED
@@ -60,38 +59,30 @@ class EM():
 			sigma = np.sqrt(sigmasqr) #this sigma value is passed to next iteration
 			
 			# checking if it satisfies eq 1 and concluding whether its from Model 1 or 2.
-			for i in range(0,len(img[x][y])):
-				if img[x][y][i] == EM.convolve(img)[i]:
-					print(1)
-					return img
-					pass # belongs to M1
-				else:
-					return img
-					pass # belongs to M2
+			for x,y in COORDINATES:
+				isum = EM.convolve(img, x, y, ALPHA, K)
+				for i in range(0,len(img[x][y])):
+					if img[x][y][i] == isum[x][y][i]:
+						pass # belongs to M1
+					else:
+						pass # belongs to M2
 
 		
 			# do eq 7
 
 			# END OF EM Algorithm
-		'''
-	def convolve(img):
-		SIGMAZERO,R,P,W,K,PZERO,NOT_USED_COORDS,ALPHA = Initialize.all(img)
 
-		indices = np.where(img != [0])
-		COORDINATES = zip(indices[0], indices[1])
-
-		for x,y in COORDINATES:
-			isum = 0
-			for p in range(-ALPHA,ALPHA):
-				try:
-					img[x + p][y + p]
-				except IndexError:
-					# this error is raised when kernel reaches an edge of the image, stopping the loop.
-					break
-				# this is the Equation No. 1 from the paper.
-				isum += K[p][p] * img[x + p][y + p]
-
-			img[x][y] = isum
+	def convolve(img, x, y, ALPHA, K):
+		isum = 0
+		for p in range(-ALPHA,ALPHA):
+			try:
+				img[x + p][y + p]
+			except IndexError:
+				# this error is raised when kernel reaches an edge of the image, stopping the loop.
+				break
+			# this is the Equation No. 1 from the paper.
+			isum += K[p][p] * img[x + p][y + p]
+		img[x][y] = isum
 		
 		# returns convulted image.
 		return img
@@ -105,13 +96,13 @@ class GD():
 		p = []
 		for i in range(0,len(x)):
 			_z.append((x[i] / sigma)**2)
-			if (_z[i]) > 709:
-				 _z[i] = 0 # done to avoid fp precision issues.
+			if (_z[i]).any() > 709:
+				 _z[i] = [0,0,0] # done to avoid fp precision issues.
 			_exp.append(np.exp(_z[i]))
 			_b = 2 * np.pi * sigma**2
 			p.append(1 / np.sqrt(_b * _exp[i]))
 
-		return p
+		return p[0]
 
 if __name__ == '__main__':
 	img = cv.imread('../data/Fake/ex.png')
