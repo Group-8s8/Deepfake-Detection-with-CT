f89c56029c9b3b042f8927e168f6d0a8e522398b
Nuzair Rasheed <38219313+Nuzair46@users.noreply.github.com>
2021-06-01 20:13:03 +0530

    Update read.py

--- read.py
+++ read.py
@@ -132,6 +132,7 @@ class Normal():
 		# Image is normalized to values between 0 and 1 so that calculations will be easier.
 		pixels = asarray(img)
 		pixels = pixels.astype('float32')
+		# 255 is max value. so (any number < 255)/255 => 0 < number < 1
 		pixels /= 255.0
 		
 		return pixels
